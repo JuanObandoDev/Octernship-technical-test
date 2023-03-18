242449e89ea026aaea4ba04a0468711fd71fc3f5
Juan Obando <juanobando.developer@gmail.com>
2023-03-17 21:05:22 -0500

    feat: add components to principal

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { FirstPage } from "./components/FirstPage";
+import { SecondPage } from "./components/SecondPage";
+import { useState } from "react";
+import { useValidateInput } from "./components/hooks/useValidateInput";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [valid, setValid] = useState(false);
+
+  const validateInput = () => {
+    if (useValidateInput(value)) {
+      setValid(true);
+    } else {
+      alert("provide a non-empty value");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="pagesBtns">
+        <button
+          onClick={() => {
+            setValid(false);
+            setValue("");
+          }}
+          className={`firstPage ${valid ? "" : "active"}`}
+          disabled={!valid}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          First Page
+        </button>
+        <button
+          onClick={validateInput}
+          className={`secondPage ${valid ? "active" : ""}`}
+          disabled={valid}
+        >
+          Second Page
+        </button>
+      </div>
+      {valid ? (
+        <>
+          <SecondPage value={value} setValue={setValue} />
+        </>
+      ) : (
+        <>
+          <FirstPage setValid={setValid} value={value} setValue={setValue} />
+        </>
+      )}
+    </>
   );
 }
 
