d1bc25088234d5a2bf2ca8647c3b946db8919b73
Juan Obando <juanobando.developer@gmail.com>
2023-03-23 21:13:29 -0500

    refactor: moving hooks from components to src folder

--- app/src/components/hooks/useDeleteCopy.js
+++
@@ -1,9 +0,0 @@
-export const useDeleteCopy = (item, index, value, setValue) => {
-  let valueArr = value.split("");
-  for (let i = 0; i < valueArr.length; i++) {
-    if (item === valueArr[i] && index !== i) {
-      valueArr[i] = null;
-    }
-  }
-  setValue(valueArr.join(""));
-};
--- app/src/components/hooks/useFindCopy.js
+++
@@ -1,8 +0,0 @@
-export const useFindCopy = (item, index, value) => {
-  for (let i = 0; i < value.length; i++) {
-    if (item === value[i] && index !== i) {
-      return true;
-    }
-  }
-  return false;
-};
