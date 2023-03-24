4e479ed24e123c3af1e45d24ef3b9c4b9b982ff3
Juan Obando <juanobando.developer@gmail.com>
2023-03-23 21:11:09 -0500

    refactor: make more simple app component and apply context

--- app/src/App.js
+++ app/src/App.js
@@ -1,53 +1,15 @@
 import "./styles/App.css";
 import React from "react";
-import { FirstPage } from "./components/FirstPage";
-import { SecondPage } from "./components/SecondPage";
-import { useState } from "react";
-import { useValidateInput } from "./components/hooks/useValidateInput";
+import { InputContextProvider } from "./context/inputContext";
+import { Header } from "./components/Header";
+import { Pages } from "./components/Pages";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [valid, setValid] = useState(false);
-
-  const validateInput = () => {
-    if (useValidateInput(value)) {
-      setValid(true);
-    } else {
-      alert("provide a non-empty value");
-    }
-  };
-
   return (
-    <>
-      <div className="pagesBtns">
-        <button
-          onClick={() => {
-            setValid(false);
-            setValue("");
-          }}
-          className={`firstPage ${valid ? "" : "active"}`}
-          disabled={!valid}
-        >
-          First Page
-        </button>
-        <button
-          onClick={validateInput}
-          className={`secondPage ${valid ? "active" : ""}`}
-          disabled={valid}
-        >
-          Second Page
-        </button>
-      </div>
-      {valid ? (
-        <>
-          <SecondPage value={value} setValue={setValue} />
-        </>
-      ) : (
-        <>
-          <FirstPage setValid={setValid} value={value} setValue={setValue} />
-        </>
-      )}
-    </>
+    <InputContextProvider>
+      <Header />
+      <Pages />
+    </InputContextProvider>
   );
 }
 
