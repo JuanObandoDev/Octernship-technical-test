c67fae6a8ff6c1b6152bdf37e30ee66c7b0394ce
Juan Obando <juanobando.developer@gmail.com>
2023-04-26 19:57:30 -0500

    fix: move project into main to correct publish

--- app/src/hooks/useDeleteCopy/useDeleteCopy.js
+++
@@ -1,9 +0,0 @@
-export const UseDeleteCopy = (item, index, value, setValue) => {
-  let valueArr = value.split("");
-  for (let i = 0; i < valueArr.length; i++) {
-    if (item === valueArr[i] && index !== i) {
-      valueArr[i] = null;
-    }
-  }
-  setValue(valueArr.join(""));
-};
--- app/src/hooks/useValidateInput/useValidateInput.js
+++
@@ -1,3 +0,0 @@
-export const UseValidateInput = (value) => {
-  return Boolean(value.replace(/\s/g, "").length);
-};
