eacd9492d3aae29b784c659810e51f7d59c926ff
Juan Obando <juanobando.developer@gmail.com>
2023-04-29 13:57:23 -0500

    refactor: unify react and useContext in one import and remove invisible tags

--- src/components/Pags/Pages.jsx
+++ src/components/Pags/Pages.jsx
@@ -1,22 +1,9 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { inputContext } from "../../context/inputContext/inputContext";
 import { FirstPage } from "../FirstPage/FirstPage";
 import { SecondPage } from "../SecondPage/SecondPage";
 
 export function Pages() {
   const { valid } = useContext(inputContext);
-  return (
-    <>
-      {valid ? (
-        <>
-          <SecondPage />
-        </>
-      ) : (
-        <>
-          <FirstPage />
-        </>
-      )}
-    </>
-  );
+  return (valid ? <SecondPage /> : <FirstPage />);
 }
