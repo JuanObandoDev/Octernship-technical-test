b00b93e2f4f84c8e93af4cd6dbdbd661c9d8fdb5
Juan Obando <juanobando.developer@gmail.com>
2023-04-29 15:27:15 -0500

    refactor: organize imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import "./styles/App.css";
 import React from "react";
+
 import { InputContextProvider } from "./context/inputContext/inputContext";
 import { Header } from "./components/Header/Header";
 import { Pages } from "./components/Pags/Pages";
+
+import "./styles/App.css";
 
 function App() {
   return (
